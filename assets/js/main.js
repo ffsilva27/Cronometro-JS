e30fc74d985d11650c1d88e50d38c2f0e45ff623
ffsilva27 <filipeferreiradasilva@hotmail.com>
2022-02-02 11:49:49 -0300

    Cronômetro criado com JS Vanilla durante meus estudos iniciais sobre a linguagem.

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+function relogioPrincipal(){
+  function criaRelogio(segundos){
+    //A multiplicação por 1000 é referente a unidade de medida que é em milésimo de segundos.
+    const contador = new Date(segundos*1000)
+    return contador.toLocaleTimeString('pt-BR',{
+      hour12: false,
+      timeZone: 'UTC'
+    })
+  }
+  const relogio = document.querySelector('.relogio')
+  let segundos = 0;
+  let timer;
+
+  function iniciaRelogio(){
+    timer = setInterval(function(){
+      segundos++;
+      relogio.innerHTML = criaRelogio(segundos);
+    },1000)
+  }
+
+  document.addEventListener('click',function(e){
+    const el = e.target;
+    if(el.classList.contains('zerar')){
+      clearInterval(timer);
+      relogio.innerHTML='00:00:00'
+      segundos=0;
+      relogio.classList.remove('pausado')
+    }
+    if(el.classList.contains('iniciar')){
+      clearInterval(timer)
+      relogio.classList.remove('pausado')
+      iniciaRelogio();
+    }
+    if(el.classList.contains('pausar')){
+      clearInterval(timer)
+      relogio.classList.add('pausado')
+    }
+  })
+}
+
+relogioPrincipal();
